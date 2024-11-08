7f976081a03f3e9520bbc68a98788a4ca6448761
Brendareginasilva <105654751+Brendareginasilva@users.noreply.github.com>
2024-11-08 10:38:33 -0300

    .

--- informacoesglobais.js
+++ informacoesglobais.js
@@ -3,5 +3,12 @@ const url = "https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-g
  function vizualizarinformacoesglobais(s) {
     const res = await fetch (url)
   const dados = res.jason()
-  console.log(dados)  
-}
+ const paragrafo = document.createElement("p")  
+ paragrafo.classList.add('graficos-container__texto')
+ paragrafo.innerHTML=`você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${totsl_pessoas_conectadasem alguma rede social e passam mèdia ${ dados.tempo_medio}` 
+  horas conectadas.}
+  console.log(paragrafo);
+  const.container= document 
+ }
+
+ vizualizarinformacoesglobais()
